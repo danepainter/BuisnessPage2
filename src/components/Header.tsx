91e9b3ba02dacbe884d0308f4e7d3272a71c415a
Dane Shaw <danepainter1@gmail.com>
2025-11-17 22:32:17 -0500

    Header frame

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className = 'header_container'>
+      <h1 className = 'header_title'>Hello World</h1>
+    </div>
+  )
+}
+
+export default Header;

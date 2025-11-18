04e1052fab1838f98f68e6e3060fdea74441bbeb
danepainter <danepainter1@gmail.com>
2025-11-17 23:24:13 -0500

    Hero section started

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+import './Hero.css'
+
+function Hero() {
+    return (
+        <div className = 'hero_container'>
+            <h1 className = 'hero_title'>
+                Welcome to our business page
+            </h1>
+            <p className = 'hero_description'>
+                We are a trusted partner dedicated to delivering exceptional service and innovative solutions 
+                that help our clients achieve their goals. With years of experience and a commitment to excellence, 
+                we provide tailored services designed to meet your unique needs and drive real results for your business.
+            </p>
+        </div>
+    )
+};
+
+export default Hero;

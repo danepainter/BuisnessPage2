fd156e526a818eb18f0d0acdd884a4a546a3c446
Dane Shaw <danepainter1@gmail.com>
2025-11-17 22:50:55 -0500

    Logo added with display: flex for alignment of text preceding it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import './Header.css'
+import logo from '../assets/logo.png'
+
 
 function Header() {
   return (
     <div className = 'header_container'>
-      <h1 className = 'header_title'>Hello World</h1>
+        <div className = 'header_logo'>
+            <img src = {logo} alt = 'logo' />
+        </div>
+      <h1 className = 'header_title'>
+        Hello World</h1>
     </div>
   )
 }
